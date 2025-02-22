d0f012db8086cb09ac2f9ca1b407946e11662823
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-02-22 17:33:42 +0900

    feat(theme): Add tokyo_night theme

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,9 @@
+import theme from "./theme.js"
+
 export default {
   hintAlign: "left",
   omnibarSuggestionTimeout: 500,
   richHintsForKeystroke: 1,
   stealFocusOnLoad: false,
+  theme: theme,
 }
---
+++ src/theme.js
@@ -0,0 +1,38 @@
+// tokyonight theme
+export default `
+.sk_theme {
+    font-family: Input Sans Condensed, Charcoal, sans-serif;
+    font-size: 10pt;
+    background: #1a1b26;
+    color: #c0caf5;
+}
+.sk_theme tbody {
+    color: #c0caf5;
+}
+.sk_theme input {
+    color: #c0caf5;
+}
+.sk_theme .url {
+    color: #7aa2f7;
+}
+.sk_theme .annotation {
+    color: #9aa5ce;
+}
+.sk_theme .omnibar_highlight {
+    color: #bb9af7;
+}
+.sk_theme .omnibar_timestamp {
+    color: #e0af68;
+}
+.sk_theme .omnibar_visitcount {
+    color: #9ece6a;
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+    background: #24283b;
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+    background: #3b4261;
+}
+#sk_status, #sk_find {
+    font-size: 20pt;
+}`;
