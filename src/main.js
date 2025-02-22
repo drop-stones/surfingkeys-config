2d7903db84b7b4e76dc1c8e265ce765063a75673
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-02-22 17:33:24 +0900

    feat(settings): Add settings.js

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import conf_settings from "./settings.js"
+
+// settings
+Object.assign(settings, conf_settings)
---
+++ src/settings.js
@@ -0,0 +1,6 @@
+export default {
+  hintAlign: "left",
+  omnibarSuggestionTimeout: 500,
+  richHintsForKeystroke: 1,
+  stealFocusOnLoad: false,
+}
