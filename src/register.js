54f04b99fa9dda4be8890063a5a2d0083fc8fc47
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-02-27 20:57:32 +0900

    feat(register): Separate register functions in register.js

---
+++ src/register.js
@@ -0,0 +1,77 @@
+const registerKey = (keymap, mode, domain) => {
+  const dom = (domain === "global") ? null : domain
+  if (keymap.hasOwnProperty("new_keystroke") && keymap.hasOwnProperty("old_keystroke")) {
+    const annotation = keymap.hasOwnProperty("annotation") ? keymap.annotation : null
+    if (mode === "normal") {
+      api.map(keymap.new_keystroke, keymap.old_keystroke, dom, annotation)
+    } else if (mode === "insert") {
+      api.imap(keymap.new_keystroke, keymap.old_keystroke, dom, annotation)
+    } else if (mode === "visual") {
+      api.vmap(keymap.new_keystroke, keymap.old_keystroke, dom, annotation)
+    }
+  } else if (keymap.hasOwnProperty("keys") && keymap.hasOwnProperty("annotation") && keymap.hasOwnProperty("jscode")) {
+    if (mode === "normal") {
+      api.mapkey(keymap.keys, keymap.annotation, keymap.jscode, { domain: dom })
+    } else if (mode === "insert") {
+      api.imapkey(keymap.keys, keymap.annotation, keymap.jscode, { domain: dom })
+    } else if (mode === "visual") {
+      api.vmapkey(keymap.keys, keymap.annotation, keymap.jscode, { domain: dom })
+    }
+  }
+}
+
+const unregisterKey = (keystroke, mode, domain) => {
+  const dom = (domain === "global") ? null : domain
+  if (mode === "normal") {
+    api.unmap(keystroke, dom)
+  } else if (mode === "insert") {
+    api.iunmap(keystroke, dom)
+  } else if (mode === "visual") {
+    api.vunmap(keystroke, dom)
+  }
+}
+
+const registerSearchEngine = (search_engine, search_leader) => {
+  api.addSearchAlias(
+    search_engine.alias,
+    search_engine.prompt,
+    search_engine.search_url,
+    "", // search_leader_key from clipboard
+    search_engine.suggestion_url,
+    search_engine.callback_to_parse_suggestion,
+    undefined, // only_this_site_key
+    null,
+  )
+}
+
+const registerKeys = (maps) => {
+  Object.entries(maps).forEach(([domain, maps]) => {
+    Object.entries(maps).forEach(([mode, keystrokes]) => {
+      keystrokes.forEach((keystroke) => {
+        registerKey(keystroke, mode, domain)
+      })
+    })
+  })
+}
+
+const unregisterKeys = (unmaps) => {
+  Object.entries(unmaps).forEach(([domain, maps]) => {
+    Object.entries(maps).forEach(([mode, keystrokes]) => {
+      keystrokes.forEach((keystroke) => {
+        unregisterKey(keystroke, mode, domain)
+      })
+    })
+  })
+}
+
+const registerSearchEngines = (search_engines) => {
+  search_engines.forEach((search_engine) => {
+    registerSearchEngine(search_engine, "o")
+  })
+}
+
+export default {
+  registerKeys,
+  unregisterKeys,
+  registerSearchEngines,
+}
