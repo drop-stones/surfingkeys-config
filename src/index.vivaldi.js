b75fd5dd3d34af6613656d578beaeee2de0b4a89
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-03-09 11:24:27 +0900

    feat(keymap): Cleanup keymaps

--- src/index.vivaldi.js
+++ src/index.vivaldi.js
@@ -2,18 +2,6 @@ import conf_settings from "./settings.js"
 import keys from "./keys.js"
 import search_engines from "./search-engines.js"
 import register from "./register.js"
-import utils from "./utils.js"
-
-// vivaldi-specific settings
-const unmaps = {
-  global: {
-    normal: [
-      "H", // Open opened URL in current tab
-      "x", // Close current tab
-    ]
-  }
-}
-keys.unmaps = utils.mergeMaps(keys.unmaps, unmaps);
 
 // settings
 Object.assign(settings, conf_settings);
@@ -24,3 +12,19 @@ register.registerKeys(keys.maps);
 
 // search engines
 register.registerSearchEngines(search_engines);
+
+// vivaldi-specific settings
+const unmaps = {
+  global: {
+    normal: [
+      // #3 Tabs
+      "H", // Go one tab left
+      "L", // Go one tab right
+      "x", // Close current tab
+      "<", // Move current tab to left
+      ">", // Move current tab to right
+    ]
+  }
+}
+
+register.unregisterKeys(unmaps);
--- src/keys.js
+++ src/keys.js
@@ -1,12 +1,98 @@
 import { unmaps as priv_unmaps } from "./private.js"
 import { maps as priv_maps } from "./private.js"
-const { RUNTIME } = api
 import utils from "./utils.js"
 
 // Remove unused default mappings
 var unmaps = {
   global: {
-    normal: ["e", "E", "R", "S", "D"],
+    normal: [
+      // #1: Mouse Click
+      ";m", // mouse out last element
+      ";fs", // Display hints to focus scrollable elements
+      ";di", // Download image
+      "<Ctrl-h>", // Mouse over elements
+      "<Ctrl-j>", // Mouse out elements
+      "<Ctrl-i>", // Go to edit box with vim editor
+      // #2: Scroll Page / Element
+      "e", // Scroll half page up
+      // #3: Tabs
+      "E", // Go one tab left
+      "R", // Go one tab right
+      "<<", // Move current tab to left
+      ">>", // Move current tab to right
+      "yt", // Duplicate current tab
+      "yT", // Duplicate current tab in background
+      "g0", // Go to the first tab
+      "g$", // Go to the last tab
+      "gx0", // Close all tabs on left
+      "gxt", // Close tab on left
+      "gxT", // Close tab on right
+      "gx$", // Close all tabs on right
+      "gxx", // Close all tabs except current one
+      "gxp", // Close playing tab
+      ";gt", // Gather filtered tabs into current window
+      ";gw", // Gather all tabs into current window
+      // #4: Page Navigation
+      "gT", // Go to first activated tab
+      "gt", // Go to last activated tab
+      "g?", // Reload current page without query string(all parts after question mark)
+      "g#", // Reload current page without hash fragment
+      "U", // Scroll full page up
+      "P", // Scroll full page down
+      "%", // Scroll to percentage of current page
+      ";w", // Focus top window
+      "<Ctrl-6>", // Go to last used tab
+      // #5: Sessions
+      "ZZ", // Save session and quit
+      "ZR", // Restore last session
+      // #6: Search selected with
+      "sd", // Search selected with duckduckgo
+      "se", // Search selected with wikipedia
+      "sb", // Search selected with baidu
+      "sw", // Search selected with bing
+      "ss", // Search selected with stackoverflow
+      // #7: Clipboard
+      "yG", // Capture current full page
+      "yg", // Capture current page
+      "yS", // Capture scrolling element
+      "yj", // Copy current settings
+      "yl", // Copy current page's title
+      "yQ", // Copy all query history of OmniQuery.
+      "yf", // Copy form data in JSON on current page
+      "yp", // Copy form data for POST on current page
+      "yd", // Copy current downloading URL
+      "yc", // Copy a column of a table
+      "yq", // Copy pre text
+      "ys", // Copy current page's source
+      "yY", // Copy all tabs's url
+      "yh", // Copy current page's host
+      "cq", // Query word with Hints
+      ";pp", // Paste html on current page
+      ";pj", // Restore settings data from clipboard
+      ";pf", // Fill form with data from yf
+      ";cq", // Clear all URLs in queue to be opened
+      // #8: Omnibar
+      "ob", // Open omnibar for baidu search
+      "od", // Open omnibar for duckduckgo search
+      "ow", // Open omnibar for bing search
+      "oe", // Open Omnibar for wikipedia Search
+      "os", // Open Omnibar for stackoverflow Search
+      // #12: Chrome URLs
+      "gc", // Open Chrome Cache
+      ";j", // Close Downloads Shelf
+      // #13: Proxy
+      "cp", // Toggle proxy for current site
+      ";pa", // set proxy mode `always`
+      ";pb", // set proxy mode `byhost`
+      ";pd", // set proxy mode `direct`
+      ";ps", // set proxy mode `system`
+      ";pc", // set proxy mode `clear`
+      ";cp", // Copy proxy info
+      ";ap", // Apply proxy info from clipboard
+      // #14: Misc
+      "gr", // Read selected text or text from clipboard
+      ";ph", // Put histories from clipboard
+    ],
     insert: [],
     visual: [],
   }
@@ -24,36 +110,52 @@ var maps = {
         old_keystroke: "af",
         annotation: "#1Open a link in active new tab"
       },
-      // #4: Page Navigation
+      // #3: Tabs
       {
-        keys: "J",
-        annotation: "#4Go back in history",
+        keys: "H",
+        annotation: "#3Go one tab left",
         jscode: function () {
-          history.go(-1);
+          api.RUNTIME("previousTab");
         },
         options: { repeatIgnore: true },
       },
       {
-        keys: "K",
-        annotation: "#4Go forward in history",
+        keys: "L",
+        annotation: "#3Go one tab right",
         jscode: function () {
-          history.go(1);
+          api.RUNTIME("nextTab");
+        },
+        options: { repeatIgnore: true },
+      },
+      {
+        keys: "<",
+        annotation: "#3Move current tab to left",
+        jscode: function () {
+          api.RUNTIME('moveTab', { step: -1 })
+        },
+        options: { repeatIgnore: true },
+      },
+      {
+        keys: ">",
+        annotation: "#3Move current tab to right",
+        jscode: function () {
+          api.RUNTIME('moveTab', { step: 1 })
         },
         options: { repeatIgnore: true },
       },
     ],
     insert: [],
     visual: [],
+    omnibar: [],
   },
-  // domain-specific mappings
 }
 
 if (priv_unmaps) {
-  unmaps = mergeMaps(unmaps, priv_unmaps);
+  unmaps = utils.mergeMaps(unmaps, priv_unmaps);
 }
 
 if (priv_maps) {
-  maps = mergeMaps(maps, priv_maps);
+  maps = utils.mergeMaps(maps, priv_maps);
 }
 
 export default {
