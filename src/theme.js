dd2012884101f0b836fbb50360b78e69f2fde376
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-03-13 22:22:33 +0900

    feat(theme): Change to dark theme

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,15 @@
-// tokyonight theme
 export default `
 .sk_theme {
     font-family: Input Sans Condensed, Charcoal, sans-serif;
     font-size: 10pt;
-    background: #1a1b26;
-    color: #c0caf5;
+    background: #181818e0;
+    color: #E0E0E0;
 }
 .sk_theme tbody {
-    color: #c0caf5;
+    color: #E0E0E0;
 }
 .sk_theme input {
-    color: #c0caf5;
+    color: #E0E0E0;
 }
 .sk_theme .url {
     color: #7aa2f7;
@@ -28,11 +27,13 @@ export default `
     color: #9ece6a;
 }
 .sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
-    background: #24283b;
+    background: #303030e0;
 }
 .sk_theme #sk_omnibarSearchResult ul li.focused {
-    background: #3b4261;
+    background: #515151e0;
 }
 #sk_status, #sk_find {
     font-size: 20pt;
-}`;
+    background: #242424e0;
+    color: #E0E0E0;
+}
